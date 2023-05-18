9114dc1a314841a6ba9990b06ad9b1098a1aedb7
ZakZanko <zakzanko2112@gmail.com>
2023-05-18 16:29:38 +0300

    fixed

--- js/task-07.js
+++ js/task-07.js
@@ -2,7 +2,7 @@ const refs = {
   inputEl: document.querySelector("#font-size-control"),
   textEl: document.querySelector("#text"),
 };
-
+refs.textEl.style.fontSize = `55px`;
 refs.inputEl.addEventListener("input", Size);
 
 function Size(ev) {
